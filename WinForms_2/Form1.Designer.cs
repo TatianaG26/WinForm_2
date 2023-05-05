bc46a0f108fc294be15acaaf357b524cde70b0ca
TayianaG26 <ttolpygo@gmail.com>
2023-05-05 22:49:44 +0300

    Добавьте файлы проекта.

---
+++ WinForms_2/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WinForms_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Surname = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_Patronymic = new System.Windows.Forms.TextBox();
+            this.textBox_Sex = new System.Windows.Forms.TextBox();
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.textBox_Info = new System.Windows.Forms.TextBox();
+            this.label_Surname = new System.Windows.Forms.Label();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_Patronymic = new System.Windows.Forms.Label();
+            this.label_Gender = new System.Windows.Forms.Label();
+            this.label_birth = new System.Windows.Forms.Label();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label_info = new System.Windows.Forms.Label();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_Load = new System.Windows.Forms.Button();
+            this.textBox_Day = new System.Windows.Forms.TextBox();
+            this.textBox_Month = new System.Windows.Forms.TextBox();
+            this.textBox_Year = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Surname
+            // 
+            this.textBox_Surname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Surname.Location = new System.Drawing.Point(260, 41);
+            this.textBox_Surname.Name = "textBox_Surname";
+            this.textBox_Surname.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Surname.TabIndex = 7;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Name.Location = new System.Drawing.Point(260, 69);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Name.TabIndex = 8;
+            // 
+            // textBox_Patronymic
+            // 
+            this.textBox_Patronymic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Patronymic.Location = new System.Drawing.Point(260, 97);
+            this.textBox_Patronymic.Name = "textBox_Patronymic";
+            this.textBox_Patronymic.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Patronymic.TabIndex = 9;
+            // 
+            // textBox_Sex
+            // 
+            this.textBox_Sex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Sex.Location = new System.Drawing.Point(260, 125);
+            this.textBox_Sex.Name = "textBox_Sex";
+            this.textBox_Sex.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Sex.TabIndex = 10;
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Status.Location = new System.Drawing.Point(260, 223);
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Status.TabIndex = 12;
+            // 
+            // textBox_Info
+            // 
+            this.textBox_Info.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox_Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Info.Location = new System.Drawing.Point(260, 253);
+            this.textBox_Info.Name = "textBox_Info";
+            this.textBox_Info.Size = new System.Drawing.Size(341, 26);
+            this.textBox_Info.TabIndex = 13;
+            // 
+            // label_Surname
+            // 
+            this.label_Surname.AutoSize = true;
+            this.label_Surname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Surname.Location = new System.Drawing.Point(88, 41);
+            this.label_Surname.Name = "label_Surname";
+            this.label_Surname.Size = new System.Drawing.Size(87, 20);
+            this.label_Surname.TabIndex = 16;
+            this.label_Surname.Text = "Фамилия";
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_name.Location = new System.Drawing.Point(88, 69);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(42, 20);
+            this.label_name.TabIndex = 17;
+            this.label_name.Text = "Имя";
+            // 
+            // label_Patronymic
+            // 
+            this.label_Patronymic.AutoSize = true;
+            this.label_Patronymic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Patronymic.Location = new System.Drawing.Point(88, 97);
+            this.label_Patronymic.Name = "label_Patronymic";
+            this.label_Patronymic.Size = new System.Drawing.Size(91, 20);
+            this.label_Patronymic.TabIndex = 18;
+            this.label_Patronymic.Text = "Отчество";
+            // 
+            // label_Gender
+            // 
+            this.label_Gender.AutoSize = true;
+            this.label_Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Gender.Location = new System.Drawing.Point(88, 125);
+            this.label_Gender.Name = "label_Gender";
+            this.label_Gender.Size = new System.Drawing.Size(42, 20);
+            this.label_Gender.TabIndex = 19;
+            this.label_Gender.Text = "Пол";
+            // 
+            // label_birth
+            // 
+            this.label_birth.AutoSize = true;
+            this.label_birth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_birth.Location = new System.Drawing.Point(88, 177);
+            this.label_birth.Name = "label_birth";
+            this.label_birth.Size = new System.Drawing.Size(141, 20);
+            this.label_birth.TabIndex = 20;
+            this.label_birth.Text = "Дата рождения";
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_status.Location = new System.Drawing.Point(88, 223);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(156, 20);
+            this.label_status.TabIndex = 21;
+            this.label_status.Text = "Семейный статус";
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_info.Location = new System.Drawing.Point(88, 253);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(159, 20);
+            this.label_info.TabIndex = 22;
+            this.label_info.Text = "Доп. информация";
+            // 
+            // button_Save
+            // 
+            this.button_Save.BackColor = System.Drawing.Color.Lime;
+            this.button_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Save.Location = new System.Drawing.Point(105, 321);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(150, 73);
+            this.button_Save.TabIndex = 23;
+            this.button_Save.Text = "Save";
+            this.button_Save.UseVisualStyleBackColor = false;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // button_Load
+            // 
+            this.button_Load.BackColor = System.Drawing.Color.Lime;
+            this.button_Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Load.Location = new System.Drawing.Point(436, 321);
+            this.button_Load.Name = "button_Load";
+            this.button_Load.Size = new System.Drawing.Size(150, 73);
+            this.button_Load.TabIndex = 24;
+            this.button_Load.Text = "Load";
+            this.button_Load.UseVisualStyleBackColor = false;
+            this.button_Load.Click += new System.EventHandler(this.button_Load_Click);
+            // 
+            // textBox_Day
+            // 
+            this.textBox_Day.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Day.Location = new System.Drawing.Point(308, 177);
+            this.textBox_Day.Name = "textBox_Day";
+            this.textBox_Day.Size = new System.Drawing.Size(49, 26);
+            this.textBox_Day.TabIndex = 25;
+            // 
+            // textBox_Month
+            // 
+            this.textBox_Month.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Month.Location = new System.Drawing.Point(430, 177);
+            this.textBox_Month.Name = "textBox_Month";
+            this.textBox_Month.Size = new System.Drawing.Size(51, 26);
+            this.textBox_Month.TabIndex = 26;
+            // 
+            // textBox_Year
+            // 
+            this.textBox_Year.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Year.Location = new System.Drawing.Point(528, 177);
+            this.textBox_Year.Name = "textBox_Year";
+            this.textBox_Year.Size = new System.Drawing.Size(75, 26);
+            this.textBox_Year.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(250, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "День";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(362, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Месяц";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(483, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Год";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(702, 433);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Year);
+            this.Controls.Add(this.textBox_Month);
+            this.Controls.Add(this.textBox_Day);
+            this.Controls.Add(this.button_Load);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.label_birth);
+            this.Controls.Add(this.label_Gender);
+            this.Controls.Add(this.label_Patronymic);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.label_Surname);
+            this.Controls.Add(this.textBox_Info);
+            this.Controls.Add(this.textBox_Status);
+            this.Controls.Add(this.textBox_Sex);
+            this.Controls.Add(this.textBox_Patronymic);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_Surname);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Surname;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_Patronymic;
+        private System.Windows.Forms.TextBox textBox_Sex;
+        private System.Windows.Forms.TextBox textBox_Status;
+        private System.Windows.Forms.TextBox textBox_Info;
+        private System.Windows.Forms.Label label_Surname;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_Patronymic;
+        private System.Windows.Forms.Label label_Gender;
+        private System.Windows.Forms.Label label_birth;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label_info;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Button button_Load;
+        private System.Windows.Forms.TextBox textBox_Day;
+        private System.Windows.Forms.TextBox textBox_Month;
+        private System.Windows.Forms.TextBox textBox_Year;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WinForms_2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WinForms_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [OnSerializing]
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int day = int.Parse(textBox_Day.Text);
+                int month = int.Parse(textBox_Month.Text);
+                int year = int.Parse(textBox_Year.Text);
+            
+                Info info = new Info
+                {
+                    Surname = textBox_Surname.Text,
+                    Name = textBox_Name.Text,
+                    Patronymic = textBox_Patronymic.Text,
+                    Sex = textBox_Sex.Text,
+                    DateOfBirth = new DateTime(year, month, day),
+                    FamilyStatus = textBox_Status.Text,
+                    AdditionalInfo = textBox_Info.Text
+                };
+                FileStream fileStream = new FileStream("Info.xml", FileMode.Create, FileAccess.Write);
+                XmlSerializer serializer = new XmlSerializer(typeof(Info));
+                serializer.Serialize(fileStream, info);
+                fileStream.Close();
+                MessageBox.Show("Данные загружены!");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            // очищаем поля
+            textBox_Surname.Text = string.Empty;
+            textBox_Name.Text = string.Empty;
+            textBox_Patronymic.Text = string.Empty;
+            textBox_Sex.Text = string.Empty;
+            textBox_Day.Text = string.Empty;
+            textBox_Month.Text = string.Empty;
+            textBox_Year.Text = string.Empty;
+            textBox_Status.Text = string.Empty;
+            textBox_Info.Text = string.Empty;
+        }
+        private void button_Load_Click(object sender, EventArgs e)
+        {
+            Info info = new Info();
+            FileStream fileStream = new FileStream("Info.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer serializer = new XmlSerializer(typeof(Info));
+            info = (Info)serializer.Deserialize(fileStream);
+            fileStream.Close();
+            textBox_Name.Text = info.Name;
+            textBox_Surname.Text = info.Surname;
+            textBox_Patronymic.Text = info.Patronymic;
+            textBox_Sex.Text = info.Sex;
+            textBox_Status.Text = info.FamilyStatus;
+            textBox_Info.Text = info.AdditionalInfo;
+            textBox_Day.Text = info.DateOfBirth.Day.ToString();
+            textBox_Month.Text = info.DateOfBirth.Month.ToString();
+            textBox_Year.Text = info.DateOfBirth.Year.ToString();
+        }
+        [Serializable]
+        public class Info
+        {
+            public string Surname { get; set; }       // Фамилия
+            public string Name { get; set; }          // Имя
+            public string Patronymic { get; set; }    // Отчество
+            public string Sex { get; set; }           // Пол
+            public DateTime DateOfBirth { get; set; } // Год и дата рождения
+            public string FamilyStatus { get; set; }  // Семейный статус
+            public string AdditionalInfo { get; set; }// Дополнительная информация
+            public Info() { } // для сериализации должен быть конструктор не принимающий параметров
+        }
+    }
+}
